fe415e653a9912deffee798842bbf730525d66b0
Auberginees <119035370+Auberginees@users.noreply.github.com>
2022-11-25 10:01:30 +0200

    Add files via upload

---
+++ Лаб 3/Kraskal/Kraskal/Kraskal.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+//14
+
+//0 1 4
+//0 7 8
+//1 2 8
+//1 7 11
+//2 3 7
+//2 8 2
+//2 5 4
+//3 4 9
+//3 5 14
+//4 5 10
+//5 6 2
+//6 7 1
+//6 8 6
+//7 8 7
+
+using namespace std;
+
+int previous[100];
+
+int set_find(int v) {
+
+	if (previous[v] == v) {
+		return v;
+	}
+
+	return set_find(previous[v]);
+};
+
+
+void set_unite(int v, int u) {
+	int fv = set_find(v);
+	int fu = set_find(u);
+	previous[fv] = fu;
+};
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+	for (int i = 0; i < 100; i++)
+	{
+		previous[i] = i;
+	}
+
+	int v, e;
+	int first, sec, weight;
+	vector < pair < int, pair < int, int > > > edges;
+	cout << "Введiть к-сть вершин i ребер: " << endl;
+	cin >> v >> e;
+
+	cout << "Введiть : " << endl;
+
+	for (int i = 0; i < e; i++)
+	{
+		cin >> first >> sec >> weight;
+		edges.push_back(make_pair(weight, make_pair(first, sec)));
+	}
+
+	cout << endl;
+	cout << "Пiсля Алгоритму Краскала: \n";
+	int mst_weight = 0, mst_edges = 0, mst_n = 0;
+
+	sort(edges.begin(), edges.end());
+	while ((mst_edges < v - 2) || (mst_n < e))
+	{
+		first = edges[mst_n].second.first;
+		sec = edges[mst_n].second.second;
+		weight = edges[mst_n].first;
+
+		if (set_find(first) != set_find(sec)) {
+			set_unite(first, sec);
+			mst_weight += weight;
+			cout << first << "  " << sec
+				<< " -> " << weight << endl;
+			mst_edges++;
+		}
+		mst_n++;
+	}
+}
---
+++ Лаб 3/Pryma/Pryma/Pryma.cpp
@@ -0,0 +1,88 @@
+﻿#include<iostream>
+
+//0 4 0 0 0 2
+//4 0 6 0 0 3
+//0 6 0 3 0 1
+//0 0 3 0 2 0
+//0 0 0 2 0 4
+//2 3 1 0 4 0
+
+using namespace std;
+
+const int V = 6;
+
+int min_e(int key[], bool visited[])
+{
+    int min = 999, min_index;
+
+    for (int v = 0; v < V; v++) {
+        if (visited[v] == false && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+void print(int parent[], int cost[V][V])
+{
+    setlocale(LC_CTYPE, "ukr");
+    int minCost = 0;
+    cout << "\nРебра \tВага\n";
+    for (int i = 1; i < V; i++) {
+        cout << parent[i] << " - " << i << " \t" << cost[i][parent[i]] << " \n";
+        minCost += cost[i][parent[i]];
+    }
+    cout << "\nЗагальна вага " << minCost << endl;;
+}
+
+void sel_e(int cost[V][V])
+{
+    int parent[V], key[V];
+    bool visited[V];
+
+
+    for (int i = 0; i < V; i++) {
+        key[i] = 999;
+        visited[i] = false;
+        parent[i] = -1;
+    }
+
+    key[0] = 0;  
+    parent[0] = -1;
+
+    for (int x = 0; x < V - 1; x++)
+    {
+        int u = min_e(key, visited);
+
+        visited[u] = true;
+
+        for (int v = 0; v < V; v++)
+        {
+            if (cost[u][v] != 0 && visited[v] == false && cost[u][v] < key[v])
+            {
+                parent[v] = u;
+                key[v] = cost[u][v];
+            }
+        }
+    }
+
+    print(parent, cost);
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    int cost[V][V];
+    cout << "Введiть вершини графа: " << endl;
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            cin >> cost[i][j];
+        }
+    }
+    sel_e(cost);
+
+    return 0;
+}
